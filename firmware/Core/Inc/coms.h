2cc5ca83e63313b43a80e6bdb9d01bc83cf738f5
Isak Åslund <aslundisak@gmail.com>
2023-11-01 23:09:40 +0100

    feat: Coms interface

---
+++ firmware/Core/Inc/coms.h
@@ -0,0 +1,16 @@
+/*
+ * coms.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: Isak
+ */
+
+#ifndef INC_COMS_H_
+#define INC_COMS_H_
+
+
+void coms_add_rx(uint8_t c);
+void coms_add_tx(uint8_t c);
+
+
+#endif /* INC_COMS_H_ */
